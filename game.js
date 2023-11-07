160a884835533bed6092168bf3077847ec38210a
Piyushbh64 <115110560+Piyushbh64@users.noreply.github.com>
2023-11-07 18:01:07 +0530

    completed the project

--- game.js
+++ game.js
@@ -1,10 +1,18 @@
 var buttonColours = ["red","yellow", "blue", "green"];
 var gamepattern = [];
 var userClickedPattern  = [];
-var started = false; 
+let started = false;
+var level = 0;
 
-$(document).on("click",function(){
-    started.toggle();
+
+$("level-title").text("Level " + level);
+
+$(document).keydown(function(){
+    if(!started){
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
     
 })
 
@@ -12,12 +20,42 @@ $(".btn").on("click", function(){
     var userChosenColor = this.id;
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
-    console.log(userClickedPattern);
     animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel){
+    console.log(currentLevel)
+    if(gamepattern[currentLevel]===userClickedPattern[currentLevel]){
+        console.log(gamepattern[currentLevel]);
+        console.log(userClickedPattern[currentLevel]);
+        console.log("Success");
+        if(userClickedPattern.length===gamepattern.length){
+            console.log(userClickedPattern.length);
+            console.log(gamepattern.length);
+            setTimeout(() => {
+                nextSequence();
+                console.log("heheheh");
+            }, 1000);
+        }
+    }
+    else{
+        startOver();
+        playSound("wrong");
+        console.log("fails");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart ");
+    }
+}
 
 function nextSequence(){
+    userClickedPattern = [];
+    level += 1;
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random()*4);
     // console.log(buttonColours[randomNumber]);
     var randomChosenColour = buttonColours[randomNumber];
@@ -38,3 +76,8 @@ function animatePress(currentcolour){
      }, 100);
 }
 
+function startOver(){
+    gamepattern = [];
+    started = false;
+    level = 0;
+}
